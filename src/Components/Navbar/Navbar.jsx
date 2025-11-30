a7ad649c49052ed15e6ba8f478a205b7f794ad99
MenaEldeeb <menaeldeeb98@gmail.com>
2025-11-30 03:15:38 +0200

    add the last update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef } from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
@@ -9,20 +9,10 @@ import styles from "./Navbar.module.css";
 export default function Navbar() {
   const { isLogin, setLogin } = useContext(UserContext);
   const { cart } = useContext(CartContext);
-  const collapseRef = useRef(null);
 
   const handleLogout = () => {
     localStorage.removeItem("userToken");
     setLogin(null);
-    closeNavbar();
-  };
-
-  const closeNavbar = () => {
-    const collapseEl = collapseRef.current;
-    if (collapseEl && collapseEl.classList.contains("show")) {
-      const bsCollapse = window.bootstrap.Collapse.getInstance(collapseEl);
-      if (bsCollapse) bsCollapse.hide();
-    }
   };
 
   const navItems = ["Home", "Men", "Women", "Kids", "Handmade", "Carts", "Dashboard"];
@@ -30,15 +20,23 @@ export default function Navbar() {
   return (
     <nav className={`navbar navbar-expand-lg fixed-top ${styles.navbar}`}>
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/" onClick={closeNavbar}>Dream</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent">
+        <Link className="navbar-brand" to="/">Dream</Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarContent"
+          aria-controls="navbarContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarContent" ref={collapseRef}>
+        <div className="collapse navbar-collapse" id="navbarContent">
           <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
             {navItems.map(item => (
               <li className="nav-item" key={item}>
-                <Link className="nav-link" to={`/${item.toLowerCase()}`} onClick={closeNavbar}>
+                <Link className="nav-link" to={`/${item.toLowerCase()}`}>
                   {item}
                 </Link>
               </li>
@@ -49,7 +47,7 @@ export default function Navbar() {
             {isLogin ? (
               <>
                 <div className={styles.cartContainer}>
-                  <Link to="/carts" className="text-success me-3 position-relative" onClick={closeNavbar}>
+                  <Link to="/carts" className="text-success me-3 position-relative">
                     <i className="fa-solid fa-cart-shopping fa-lg"></i>
                     {cart.length > 0 && <span className={styles.badge}>{cart.length}</span>}
                   </Link>
@@ -60,10 +58,10 @@ export default function Navbar() {
               </>
             ) : (
               <>
-                <Link className="btn btn-custom btn-sm me-2" to="/login" onClick={closeNavbar}>
+                <Link className="btn btn-custom btn-sm me-2" to="/login">
                   Login
                 </Link>
-                <Link className="btn btn-custom btn-sm" to="/register" onClick={closeNavbar}>
+                <Link className="btn btn-custom btn-sm" to="/register">
                   Register
                 </Link>
               </>
@@ -74,5 +72,3 @@ export default function Navbar() {
     </nav>
   );
 }
-
-
